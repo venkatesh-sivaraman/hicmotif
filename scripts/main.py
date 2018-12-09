8a8fa415ed3a652091d026af7f3453643118ab81
ayusuf223 <ayusuf@mit.edu>
2018-12-08 23:53:41 -0500

    Added notebook to test the different R init conditions

--- scripts/main.py
+++ scripts/main.py
@@ -12,18 +12,6 @@ from functools import partial
 FRACTION_OLD_R = 0.25
 CONVERGENCE_THRESHOLD = 0.05
 CONVERGENCE_NUM_ITERS = 5
-NUM_WORKERS = 4
-
-def adaptive_learning_rate(new, old):
-    """
-    Computes a sigmoid adaptive learning rate given the new score and the
-    old score.
-    """
-    a = 0.95 # Value of curve at 0
-    coef = 1.0 / old * np.log(a / (1.0 - a))
-    return 1.0 / (1.0 + np.exp(coef * (new - old)))
-    #return 1.0 - FRACTION_OLD_R #0.9 if new < old else 0.6
-
 
 def load_pickles(filename):
     """
@@ -124,34 +112,6 @@ def interpolate_state_labels(state_labels, original_spacing, new_spacing):
     f = interpolate.interp1d(np.arange(0, max_x, original_spacing), state_labels, kind='nearest')
     return f(np.arange(0, max_x - original_spacing + 1, new_spacing))
 
-def estimate_worker(Y, n_labels, seq_length, spacing, info):
-    (start, stop), (_, mat) = info
-    R = np.zeros((n_labels, n_labels))
-    counts = np.zeros((n_labels, n_labels))
-
-    print(mat.identifier)
-    r = mat.range()
-    # Get meshgrid for interaction matrix
-    loci_x, loci_y, dim = mat.meshgrid(spacing, start_val=(seq_length // spacing) * spacing, max_dim=stop - start)
-    if dim > stop - start:
-        print("Too large dimension, skipping: {} vs {} - {}".format(dim, stop, start))
-        return None, None
-    vals = mat.value_at(loci_x, loci_y)
-
-    # Get meshgrid for states
-    states_1, states_2 = np.meshgrid(Y[start:start + dim], Y[start:start + dim])
-    if np.sum(np.isnan(vals)) > 0:
-        print("Shouldn't happen")
-
-    # Update counts and interaction frequency estimates
-    for s1 in range(n_labels):
-        for s2 in range(n_labels):
-            flags = np.logical_and(states_1 == s1, states_2 == s2)
-            counts[s1, s2] += np.sum(flags)
-            R[s1, s2] += np.sum(np.where(flags, vals, 0.0))
-    return R, counts
-
-
 def estimate_pairwise_interactions(data, ranges, Y, n_labels, seq_length, spacing):
     """
     Produces a pairwise interaction matrix R of size n_labels x n_labels. Each
@@ -160,14 +120,6 @@ def estimate_pairwise_interactions(data, ranges, Y, n_labels, seq_length, spacin
     """
     R = np.zeros((n_labels, n_labels))
     counts = np.zeros((n_labels, n_labels))
-
-    # worker = partial(estimate_worker, Y, n_labels, seq_length, spacing)
-    # pool = mp.Pool(processes=NUM_WORKERS)
-    # for sub_R, sub_counts in pool.imap(worker, zip(ranges, data)):
-    #     if sub_R is None:
-    #         continue
-    #     R += sub_R
-    #     counts += sub_counts
 
     for (start, stop), (_, mat) in zip(ranges, data):
         print(mat.identifier)
@@ -202,7 +154,7 @@ def dp_worker(R, coarse_spacing, spacing, seq_length, batch_item, trim=True):
     trimmed = interpolate_state_labels(state_labels, coarse_spacing, spacing)
     return H.identifier, score, (trimmed[seq_length // spacing:] if trim else trimmed)
 
-def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_size=40, last_score=None):
+def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_size=40, old_rnn=None):
     """
     Perform one iteration of training, using the initial pairwise interaction
     matrix to generate a state labeling for each interaction matrix, then training
@@ -215,7 +167,8 @@ def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_si
     rnn_params: keyword arguments to be passed to the RNNModel constructor.
     batch_size: the number of elements of data to select randomly to use in this
         iteration.
-    last_score: the last average score, or None if this is the first iteration.
+    old_rnn: an RNNModel object, if available, whose predictions will be used
+        to bias the new DP algorithm's predicted state labels.
 
     Returns:
         * an RNNModel object trained on a batch of the given data
@@ -232,11 +185,14 @@ def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_si
     # Run DP algorithm
     coarse_spacing = spacing * 10
     Y_single = np.zeros((X.shape[0],), dtype=int) # Each element corresponds to one row of X
-    pool = mp.Pool(processes=NUM_WORKERS)
+    pool = mp.Pool(processes=4)
     worker = partial(dp_worker, R, coarse_spacing, spacing, seq_length)
-    scores = []
+    # Vectors corresponding to states for each interaction matrix
+    Y_pred = old_rnn.predict(X) if old_rnn is not None else None
+    rnn_iter = (Y_pred[start:stop] for start, stop in ranges) if old_rnn is not None else (None for _ in ranges)
+    print("Prediction composition:", np.unique(Y_pred, return_counts=True))
 
-    for i, (id, score, trimmed) in enumerate(pool.imap(worker, zip(batch, (None for _ in ranges)), chunksize=2)):
+    for i, (id, _, trimmed) in enumerate(pool.imap(worker, zip(batch, rnn_iter), chunksize=2)):
         start, stop = ranges[i]
         #TODO: Don't allow these cases to pass
         if trimmed.shape[0] < stop - start:
@@ -244,7 +200,6 @@ def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_si
             continue
         # Store predicted state labels in Y vector
         Y_single[start:stop] = trimmed[:stop - start]
-        scores.append(score / ((stop - start) * spacing))
 
     pool.close()
     pool.join()
@@ -255,16 +210,18 @@ def train_iteration(data, R, seq_length=100, spacing=10, rnn_params={}, batch_si
         Y[i,Y_single[i]] = 1
 
     print(X.shape, Y.shape)
-    print("Prediction composition on previously-trained:", np.unique(Y_single, return_counts=True))
-    new_score = np.nanmean(np.array(scores))
-    if last_score is None:
-        fraction_new_R = 0.0
+
+    '''if old_rnn is None:
+        model = rnn.RNNModel(n_labels=n_labels, n_features=X.shape[1], sequence_length=seq_length, **rnn_params)
+        model.create()
     else:
-        fraction_new_R = adaptive_learning_rate(new_score, last_score)
-        print("Scores:", new_score, last_score, fraction_new_R)
+        model = old_rnn
+    model.train(X, Y, epochs=20)
+    Y_pred = model.predict(X)'''
+    print("Prediction composition on previously-trained:", np.unique(Y_single, return_counts=True))
 
     new_R = estimate_pairwise_interactions(batch, ranges, Y_single, n_labels, seq_length, spacing)
-    return new_score, np.where(np.isnan(new_R), R, (1.0 - fraction_new_R) * R + fraction_new_R * new_R)
+    return None, np.where(np.isnan(new_R), R, FRACTION_OLD_R * R + (1.0 - FRACTION_OLD_R) * new_R)
 
 def initial_pairwise_interactions(data, n_labels, batch_size=10, bound=0.25):
     """
@@ -290,49 +247,49 @@ def initial_pairwise_interactions(data, n_labels, batch_size=10, bound=0.25):
         R = np.diag(np.random.randint(2.0, 4.0, size=n_labels))
         return R + np.random.random(size=(n_labels, n_labels)) * 2.0 + 1.0
 
-    def shear_matrix(ssm):
-        # Each column shifts one more down
-        new_ssm = np.zeros(ssm.shape)
-        for n in range(ssm.shape[0]):
-            new_ssm[:,n] = np.roll(ssm[:,n], -n)
-        return new_ssm
-
-    batch_H = [shear_matrix(data[i][1].values_grid()) for i in range(batch_size)]
-    diagonal_avg = 0
-    off_diagonal_avg = 0
-    for i in range(len(batch_H)):
-        H = batch_H[i]
-        bound_len = int(bound*H.shape[0])
-        diagonal_avg += np.average(np.concatenate((H[:bound_len//2,:], H[H.shape[0] - bound_len//2:,:])))
-        off_diagonal_avg += np.average(H[bound_len//2:H.shape[0] - bound_len//2,:])
-
-    diagonal_avg /= len(batch_H)
-    off_diagonal_avg /= len(batch_H)
-
-    R = np.full((n_labels,n_labels), off_diagonal_avg, dtype=float)
-    np.fill_diagonal(R, diagonal_avg)
-    return R
+    # def shear_matrix(ssm):
+    #     # Each column shifts one more down
+    #     new_ssm = np.zeros(ssm.shape)
+    #     for n in range(ssm.shape[0]):
+    #         new_ssm[:,n] = np.roll(ssm[:,n], -n)
+    #     return new_ssm
+    #
+    # batch_H = [shear_matrix(data[i][1].values_grid()) for i in range(batch_size)]
+    # diagonal_avg = 0
+    # off_diagonal_avg = 0
+    # for i in range(len(batch_H)):
+    #     H = batch_H[i]
+    #     bound_len = int(bound*H.shape[0])
+    #     diagonal_avg += np.average(np.concatenate((H[:bound_len//2,:], H[H.shape[0] - bound_len//2:,:])))
+    #     off_diagonal_avg += np.average(H[bound_len//2:H.shape[0] - bound_len//2,:])
+    #
+    # diagonal_avg /= len(batch_H)
+    # off_diagonal_avg /= len(batch_H)
+    #
+    # R = np.full((n_labels,n_labels), off_diagonal_avg, dtype=float)
+    # np.fill_diagonal(R, diagonal_avg)
+    # return R
 
 
 if __name__ == '__main__':
     test = len(sys.argv) > 1 and sys.argv[1] == "test"
     base = "../data/"
-    data = load_data(base + "GM12878_5k", base + "loop_sequences_GM12878.fasta", base + "epigenomic_tracks/GM12878.pickle", test=test)
+    data = load_data(base + "GM12878_10k", base + "loop_sequences_GM12878.fasta", base + "epigenomic_tracks/GM12878.pickle", test=test)
     seq_length = 100
     spacing = 50# if test else 10
     dim = int(sys.argv[-1]) if len(sys.argv) > (2 if test else 1) else 2
     Rs = [initial_pairwise_interactions(data, dim)]
-    old_score = None
+    old_rnn = None
     print("Initial:", Rs[-1])
     i = 0
     num_unchanging = 0
     while num_unchanging < CONVERGENCE_NUM_ITERS:
         print("Iteration", i)
-        new_score, R = train_iteration(data, Rs[-1], batch_size=(20 if test else 60), seq_length=seq_length, spacing=spacing, last_score=old_score)
+        new_rnn, R = train_iteration(data, Rs[-1], batch_size=(20 if test else 50), seq_length=seq_length, spacing=spacing, old_rnn=old_rnn)
         Rs.append(R)
         #if not os.path.exists("../data/rnns"): os.mkdir("../data/rnns")
         #new_rnn.model.save("../data/rnns/iteration_{}.hd5".format(i))
-        old_score = new_score
+        old_rnn = new_rnn
         print(Rs[-1])
         delta = np.nanmean(np.abs(Rs[-1] - Rs[-2]))
         print("Delta:", delta)
@@ -343,9 +300,7 @@ if __name__ == '__main__':
         i += 1
     print(Rs)
 
-    pid = os.getpid()
-    print("Writing to PID", pid)
-    out_path = base + "dp_assignments_mse/assignments_{}.csv".format(pid)
+    out_path = base + "dp_assignments/assignments_{}.csv".format(os.getpid())
     with open(out_path, 'w') as file:
         pool = mp.Pool(processes=4)
         worker = partial(dp_worker, Rs[-1], spacing * 10, spacing, seq_length, trim=False)
